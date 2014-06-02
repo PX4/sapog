c2cc9eec3b41daca166d17e359f86afe6730ce78
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-06-03 01:25:08 +0400

    Optimized default settings for spinup

--- src/motor/realtime/motor_rtctl.c
+++ src/motor/realtime/motor_rtctl.c
@@ -197,7 +197,7 @@ CONFIG_PARAM_INT("motor_spinup_timeout_ms",            1000,  100,   2000)
 CONFIG_PARAM_INT("motor_spinup_start_comm_period_usec",50000, 10000, 200000)
 CONFIG_PARAM_INT("motor_spinup_end_comm_period_usec",  2000,  1000,  10000)
 CONFIG_PARAM_INT("motor_spinup_num_good_comms",        120,   6,     1000)
-CONFIG_PARAM_FLOAT("motor_spinup_duty_cycle_inc",      0.01,  0.001, 0.1)
+CONFIG_PARAM_FLOAT("motor_spinup_duty_cycle_inc",      0.02,  0.001, 0.1)
 
 
 static void configure(void)
