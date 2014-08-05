8609113ba50e57d36e1389a572391a91a863dca3
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-08-05 14:27:15 +0400

    ChibiOS ICU workaround

--- tools/fetch_chibios.sh
+++ tools/fetch_chibios.sh
@@ -3,4 +3,6 @@
 cd $(dirname $(readlink -f $0))/../firmware
 git clone https://github.com/ChibiOS-Upstream/ChibiOS-RT.git chibios
 cd chibios
-git checkout stable_2.6.x
+#git checkout stable_2.6.x
+# ICU doesn't work at b74842655f733deb5808372e6d437a4ecfe7ad73
+git checkout f19ef0308363fd4f2d76eaac6f6dd0e1e500023b
