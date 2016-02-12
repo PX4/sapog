2d6de7479503e5a70d7a15e50c674379e0971508
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-02-13 01:47:39 +0300

    PWM input disabled by default

--- firmware/src/pwm_input.cpp
+++ firmware/src/pwm_input.cpp
@@ -57,7 +57,7 @@ chibios_rt::EvtSource _update_event;
 
 static volatile unsigned _last_pulse_width_usec;
 
-CONFIG_PARAM_BOOL("pwm_enable", true)
+CONFIG_PARAM_BOOL("pwm_enable", false)
 CONFIG_PARAM_INT("pwm_min_usec",  1000,  800, 1200)
 CONFIG_PARAM_INT("pwm_max_usec",  2000, 1800, 2200)
 
