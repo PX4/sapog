2251bd2536e434891d863224816543053e4e0ada
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-11-10 19:28:41 +0300

    Disabled the spinup safety feature by default

--- firmware/src/uavcan_node/esc_controller.cpp
+++ firmware/src/uavcan_node/esc_controller.cpp
@@ -53,7 +53,7 @@ float max_dc_to_start;
 
 os::config::Param<unsigned> param_esc_index("esc_index",           0,      0,    15);
 os::config::Param<unsigned> param_cmd_ttl_ms("cmd_ttl_ms",       200,    100,  5000);
-os::config::Param<float> param_cmd_start_dc("cmd_start_dc",     0.25,   0.01,   1.0);
+os::config::Param<float> param_cmd_start_dc("cmd_start_dc",      1.0,   0.01,   1.0);
 
 
 void cb_raw_command(const uavcan::ReceivedDataStructure<uavcan::equipment::esc::RawCommand>& msg)
