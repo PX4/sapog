c7b36a53fbc20560961811c2bbd96b00b408ceef
Pavel Kirienko <pavel.kirienko@gmail.com>
2017-02-28 16:12:21 +0300

    Updated default configuration parameters

--- firmware/src/motor/realtime/motor_rtctl.c
+++ firmware/src/motor/realtime/motor_rtctl.c
@@ -210,11 +210,11 @@ CONFIG_PARAM_INT("mot_zc_fails_max",    40,    6,     300)
 CONFIG_PARAM_INT("mot_zc_dets_min",     200,   6,     1000)
 CONFIG_PARAM_INT("mot_comm_per_max",    12000, 1000,  50000)
 // Spinup settings
-CONFIG_PARAM_INT("mot_spup_to_ms",      1000,  100,   9500)
+CONFIG_PARAM_INT("mot_spup_to_ms",      3000,  100,   9500)
 CONFIG_PARAM_INT("mot_spup_st_cp",      50000, 10000, 200000)
-CONFIG_PARAM_INT("mot_spup_en_cp",      2000,  1000,  10000)
+CONFIG_PARAM_INT("mot_spup_en_cp",      2500,  1000,  10000)
 CONFIG_PARAM_INT("mot_spup_gcomms",     200,   6,     1000)
-CONFIG_PARAM_INT("mot_spup_cp_flt",     2,     1,     15)
+CONFIG_PARAM_INT("mot_spup_cp_flt",     2,     1,     30)
 CONFIG_PARAM_FLOAT("mot_spup_dc_inc",   0.01,  0.001, 0.1)
 
 
